98aa26037ca46af23e6a5d71610d846fe5a344bc
antonmazun <kpi.study1@gmail.com>
2016-12-16 23:09:59 +0200

    bla

--- myshop/wsgi.py
+++ myshop/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
